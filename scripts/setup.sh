c3ac0d97d69eff8a4f0ff975a16277596567f4f2
jeffrey.io <website@mathgladiator.com>
2017-07-04 15:32:58 -0700

    start some thinking about how to add test sites to prod

---
+++ scripts/setup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java8 -cp /home/ec2-user/app.jar farm.bsg.SetupToolCLI $*
---
+++ src/main/java/farm/bsg/SetupToolCLI.java
@@ -0,0 +1,13 @@
+package farm.bsg;
+
+public class SetupToolCLI {
+    public static void main(String[] args) {
+        System.out.println("-=[ Setup Tool ]=-");
+        for (String arg : args) {
+            System.out.println("arg:" + arg);
+        }
+        
+        // TODO: list all customer buckets
+        // TODO: create a new customer bucket
+    }
+}
